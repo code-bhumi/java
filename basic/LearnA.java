bf7e16a997203ff7d7ddfb039357cbb858020df7
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-03-08 22:30:22 +1100

    LearnA java

---
+++ basic/LearnA.java
@@ -0,0 +1,19 @@
+// I am a single line comment
+
+/*
+  I am a 
+  mutltiline comment
+ */
+
+
+public class LearnA {
+    public static void main(String[] args) {
+        System.out.println("I am learning Java");
+        System.out.println("I am new to the java");
+        System.out.println("I like java");
+
+        System.out.print("I am a text ");
+        System.out.print("I am neighbour of the text");
+        System.out.println(" let's take this text to end");
+    }
+}
