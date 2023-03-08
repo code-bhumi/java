d93f06a3b3473b9a20218cd571a8aa1b5529a225
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-03-08 22:56:19 +1100

    learning java

---
+++ basic/LearnC.java
@@ -0,0 +1,6 @@
+public class LearnC {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 10; ++a)
+            for (int b = 1; b <= 5; ++b)
+                System.out.println(a + " + " + b + " == " + (a + b));
+}
