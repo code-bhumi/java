efb84c8603ad06db51749c3a41b381d00474f755
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-03-15 20:26:06 +1100

    addition

---
+++ basic/casting.java
@@ -0,0 +1,9 @@
+public class casting {
+    public static void main(String[] args) {
+      int myint = 15;
+      double mydouble = myint;
+      System.out.println(myint);
+      System.out.println(mydouble);  
+    }
+
+}
