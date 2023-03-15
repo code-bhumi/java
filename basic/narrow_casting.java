444e0762f18574bdfaf32e0198b09aac980b6b4a
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-03-15 20:25:38 +1100

    narrow casting

---
+++ basic/narrow_casting.java
@@ -0,0 +1,9 @@
+public class narrow_casting {
+    public static void main(String[] args) {
+         double mydouble =  4.86;
+         int myint = (int) mydouble;
+
+         System.out.println(mydouble);
+         System.out.println(myint);
+        }
+}
