9b4b35fb6c7b9562b73474a9af21693356884db1
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-03-17 22:51:00 +1100

    ternary

---
+++ basic/ternary.java
@@ -0,0 +1,8 @@
+public class ternary {
+    public static void main(String [] args) {
+        int num = -20;
+        String result;
+        result = (num > 0) ? "possitive number" : "negative number.";
+        System.out.println(result);
+    }
+}
