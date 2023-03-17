4ac083458c88b2ecfb8a6dc52003fef4bcb0357e
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-03-17 22:50:55 +1100

    vote

---
+++ basic/vote.java
@@ -0,0 +1,11 @@
+public class vote {
+    public static void main(String[] args) {
+        int age = 24;
+        if(age > 18){
+            System.out.println("yes you are eligible for vote...");
+        } else {
+            System.out.println("Sorry you can't vote:");
+        }
+
+    }
+}
