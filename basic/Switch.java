ab696d3ededce9664ad754abe54db9978cd8ea81
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-03-20 13:09:26 +1100

    switch

---
+++ basic/Switch.java
@@ -0,0 +1,24 @@
+public class Switch {
+    public static void main(String[] args) {
+        String student = "one yellow";
+        switch (student) {
+            case "one indigo":
+               System.out.println("shivangi");
+               break;
+            case "one green":
+               System.out.println("Atharv");  
+               break;                
+            case "one purple":
+               System.out.println("krishna");
+               break;
+            case "one yellow":
+               System.out.println("Adwita");
+               break;
+            case "one blue":
+               System.out.println("pearl");
+               break;
+                    
+        }
+      
+    }
+}
