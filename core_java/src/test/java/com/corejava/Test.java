16555d0f568fa316f14e00dd56fb63bfec04c336
Meenakshi Pal <pal.meenkashi@outlook.com>
2023-11-25 23:31:55 +1100

    test code

--- core_java/src/test/java/com/corejava/Test.java
+++ core_java/src/test/java/com/corejava/Test.java
@@ -2,6 +2,8 @@ package com.corejava;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World !!!");
+        for (int i = 1; i <= 11; ++i) {
+            System.out.println("i == " + i);
+        }
     }
 }
