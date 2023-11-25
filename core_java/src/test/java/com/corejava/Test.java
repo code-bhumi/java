9124f4ee71d7162f145f9815ba563888a3b1f017
Meenakshi Pal <pal.meenkashi@outlook.com>
2023-11-25 23:51:46 +1100

    test code

--- core_java/src/test/java/com/corejava/Test.java
+++ core_java/src/test/java/com/corejava/Test.java
@@ -3,7 +3,7 @@ package com.corejava;
 public class Test {
     public static void main(String[] args) {
         for (int i = 1; i <= 11; ++i) {
-            System.out.println("i == " + i);
+            System.out.println("square root of == i" + i + " == " + i * i);
         }
     }
 }
