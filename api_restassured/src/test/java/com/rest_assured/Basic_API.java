0b2bec651840e62b5d2bf2d88803b01b14e9b2b3
Meenakshi Pal <=>
2023-11-27 11:39:22 +1100

    BASIC API

--- api_restassured/src/test/java/com/rest_assured/Basic_API.java
+++ api_restassured/src/test/java/com/rest_assured/Basic_API.java
@@ -3,6 +3,8 @@ package com.rest_assured;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 
+import com.rest_assured.Files.payload;
+
 public class Basic_API {
     
     public static void main(String[] args) {
@@ -15,24 +17,7 @@ public class Basic_API {
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-        .body("{\r\n" +
-           " \"location\": {\r\n" +
-          " \"lat\": -38.383494,\r\n" +
-          " \"lng\": 33.427362\r\n" +
-          " },\r\n" +
-          " \"accuracy\": 50,\r\n" +
-          " \"name\": \"Frontline house\",\r\n" +
-          " \"phone_number\": \"(+91) 983 893 3937\",\r\n" +
-          " \"address\": \"29, side layout, cohen 09\",\r\n" +
-          " \"types\": [\r\n" +
-          " \"shoe park\",\r\n" +
-          " \"shop\"\r\n" +
-          " ],\r\n" +
-          " \"website\": \"http://google.com\",\r\n" +
-          " \"language\": \"French-IN\"\r\n" +
-          "}\r\n" +
-          "")
-          .when().post("maps/api/place/add/json")
-          .then().log().all().assertThat().statusCode(200);
+        .body(payload.AddPlace()).when().post("maps/api/place/add/json")
+        .then().log().all().assertThat().statusCode(200);
     }
 }
