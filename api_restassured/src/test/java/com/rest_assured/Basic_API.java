a64e6603216421597b5f4f7239de744370e65cac
Meenakshi Pal <=>
2023-11-28 17:43:07 +1100

    modified

--- api_restassured/src/test/java/com/rest_assured/Basic_API.java
+++ api_restassured/src/test/java/com/rest_assured/Basic_API.java
@@ -1,6 +1,8 @@
 package com.rest_assured;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 
 import com.rest_assured.Files.payload;
@@ -16,8 +18,14 @@ public class Basic_API {
         // then - Validate the response
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+        String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
         .body(payload.AddPlace()).when().post("maps/api/place/add/json")
-        .then().log().all().assertThat().statusCode(200);
+        .then().assertThat().statusCode(200).extract().response().asString();
+
+        System.out.println(response);
+        JsonPath js = new JsonPath(response);  // for parsing json
+        String placeId = js.getString("place_id");
+
+        System.out.println(placeId);
     }
 }
