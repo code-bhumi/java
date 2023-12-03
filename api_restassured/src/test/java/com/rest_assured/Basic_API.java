735f2fc891e6937c0ef4cea6dd815394708a2b27
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-12-03 11:19:38 +1100

    modified basic api

--- api_restassured/src/test/java/com/rest_assured/Basic_API.java
+++ api_restassured/src/test/java/com/rest_assured/Basic_API.java
@@ -23,14 +23,17 @@ public class Basic_API {
         .queryParam("key", "qaclick123")
         .header("Content-Type", "application/json")
         .body(payload.AddPlace()).when().post("maps/api/place/add/json")
-        .then().assertThat().statusCode(200)
-        .extract().response().asString();
+        .then().assertThat().statusCode(200).body("scope", equalTo("APP"))
+        .header("server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 
+        // Add place -> update place with new address -> get place to validate if new address is present in response
         System.out.println(response);
         JsonPath js = new JsonPath(response);  // for parsing json
         String placeId = js.getString("place_id");
 
         System.out.println(placeId);
+
+        // update place
 
         String newAddress = "Summer Walk, Australia";
 
@@ -45,17 +48,18 @@ public class Basic_API {
         .then().assertThat().log().all().statusCode(200)
         .body("msg", equalTo("Address successfully updated"));
         
-        String getPlaceResponse = given().log().all()
-        .queryParam("key", "qaclick123")
-        .queryParam("place_id",placeId)
-        .when().get("maps/api/place/add/json")
+        String getPlaceResponse = given().log().all().queryParam("key", "qaclick123")
+        .queryParam("place_id",placeId).when().get("maps/api/place/get/json")
         .then().assertThat().log().all().statusCode(200)
         .extract().response().asString();
+        System.out.println("---------------------------------------------------------------");
+        System.out.println(getPlaceResponse);
+        System.out.println("---------------------------------------------------------------");
 
         JsonPath js1 = ReusableMethod.rawtoJson(getPlaceResponse);
         String actualAddress = js1.getString("address");
         System.out.println(actualAddress);
-        Assert.assertEquals(actualAddress, "pacific ocean");
+        Assert.assertEquals(actualAddress, "Summer Walk, Australia");
 
 
     }
