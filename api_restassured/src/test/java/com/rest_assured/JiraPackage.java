75ffc070d2c6e4a9b85d80617beb0584966d2a8a
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-12-09 17:42:49 +1100

    modified

--- api_restassured/src/test/java/com/rest_assured/JiraPackage.java
+++ api_restassured/src/test/java/com/rest_assured/JiraPackage.java
@@ -5,6 +5,8 @@ import io.restassured.filter.session.SessionFilter;
 
 import static io.restassured.RestAssured.*;
 
+import java.io.File;
+
 public class JiraPackage {
     
     public static void main(String[] args) {
@@ -19,10 +21,10 @@ public class JiraPackage {
     .filter(session).when().post("/rest/auth/1/session")
     .then().log().all().extract().response().asString();
 
-    given().pathParam("id", "10200").log().all()
+    given().pathParam("id", "10300").log().all()
     .header("Content-Type","application/json")
     .body("{\r\n" +
-      "  \"body\": \"This is my first comment.\", \r\n" +
+      "  \"body\": \"This is my first comment\", \r\n" +
       "  \"visibility\": {\r\n" +
       "     \"type\": \"role\", \r\n" +
       "      \"value\": \"Administrators\"\r\n" +
@@ -31,6 +33,13 @@ public class JiraPackage {
     .filter(session).when().post("/rest/api/2/issue/{id}/comment")
     .then().log().all().assertThat().statusCode(201);
 
+    // Add Attachment
+    given().header("X-Atlassian-Token","no-check")
+    .filter(session).pathParam("id", "10300")
+    .header("Content-Type", "multipart/form-data")
+    .multiPart("file",new File("jira.txt"))
+    .when().post("rest/api/2/issue/{id}/attachments")
+    .then().log().all().assertThat().statusCode(200);
     
 
     } 
