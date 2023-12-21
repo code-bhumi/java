1c2bf1200e91fb1377e4361655409324d0f9dcaa
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-12-21 18:11:37 +1100

    serialization in rest API

---
+++ oauthrestapi/src/main/java/pojo/Location.java
@@ -0,0 +1,12 @@
+package pojo;
+
+public class Location {
+    
+    private double Lat;
+    private double Lng;
+    public void setLat(double d) {
+    }
+    public void setLng(double d) {
+    }
+   
+}
