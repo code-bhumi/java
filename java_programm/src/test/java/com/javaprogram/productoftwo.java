8ba54d4b17469228e01eaf3782b38b4629bc42ee
Meenakshi Pal <pal.meenakshi@outlook.com>
2024-03-13 13:26:23 +1100

    update

---
+++ java_programm/src/test/java/com/javaprogram/productoftwo.java
@@ -0,0 +1,15 @@
+package com.javaprogram;
+
+public class productoftwo {
+    
+    public static void main(String[] args) {
+        
+        int a = 25;
+        int b = 5;
+        int prod = 0;
+
+        prod = a*b;
+
+        System.out.println(prod);
+    }
+}
