83e14208c1085db03bc14cffe5da87c4ab24e3da
Meenakshi Pal <pal.meenakshi@outlook.com>
2024-03-15 14:26:26 +1100

    vowel check

---
+++ java_programm/src/test/java/com/javaprogram/vowel_check.java
@@ -0,0 +1,15 @@
+package com.javaprogram;
+
+public class vowel_check {
+    
+    public static void main(String[] args) 
+    {
+        System.out.println(stringContainsVowels("Apple"));
+        System.out.println(stringContainsVowels("qwth"));
+    }
+
+    public static boolean stringContainsVowels(String input) 
+    {
+        return input.toLowerCase().matches(".*[aeiou].*");
+    }
+}
